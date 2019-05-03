604a14fbba6240920416c4cac4da7db55a7193b2
Paulus Gunadi <paulus@sg.ibm.com>
2019-05-03 17:26:31 +0800

    test

--- prometheus.go
+++ prometheus.go
@@ -185,7 +185,8 @@ func IcpQuery(host string, token string, start time.Time, end time.Time, step ti
 
 func steps(step time.Duration, dur time.Duration) int {
 	if step > 0 {
-		var istep int64 = int(step.Seconds)
+		var fstep float64 = step.Seconds
+		var istep int = int(fstep)
 		return istep
 	}
 	if dur < 15*time.Minute {
